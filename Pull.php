ee2b6091ace5f6bca0c1d7627f31cdf4a3cc2f31
vlad <uplight.ca@gmail.com>
2017-02-25 21:35:36 -0500

    pull

---
+++ Pull.php
@@ -0,0 +1,19 @@
+﻿<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+$out= new stdClass();
+$log ="\n\r".date("Y-m-d h:i:s")."\n\r";
+//$log .= file_get_contents('php://input');
+//$log .= shell_exec('git fetch 2>&1');
+//sleep(5);
+//$log .= shell_exec('git reset --hard origin/master 2>&1');
+//$log .= shell_exec('git reset --hard HEAD 2>&1');
+//sleep(2);
+$log .= "end pull";
+$log .=  shell_exec('git pull 2>&1');
+$out->log = $log;
+header('Content-Type: application/json');
+echo json_encode($out);
+error_log($log, 3, "pull.log");
+?>
